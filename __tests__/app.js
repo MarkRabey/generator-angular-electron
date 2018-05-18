c2dd25a1710a387b9918e12266c8ac46c087c076
Mark Rabey <mark@markrabey.com>
2018-05-18 11:09:59 -0400

    updating tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -5,9 +5,15 @@ const helpers = require('yeoman-test');
 
 describe('generator-angular-electron:app', () => {
   beforeAll(() => {
-    return helpers
-      .run(path.join(__dirname, '../generators/app'))
-      .withPrompts({ someAnswer: true });
+    return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({
+      productName: 'My Project',
+      version: '0.1.0',
+      githubAccount: 'MarkRabey',
+      authorName: 'Mark Rabey',
+      authorEmail: 'mark@markrabey.com',
+      authorUrl: 'https://markrabey.com',
+      uikit: true,
+    });
   });
 
   it('creates files', () => {
