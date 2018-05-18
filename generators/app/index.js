309c26202ee9593a25c6a586adb2fe38dae21ab3
Mark Rabey <mark@markrabey.com>
2018-05-18 10:25:34 -0400

    building generator

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,38 +1,142 @@
 'use strict';
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
-const yosay = require('yosay');
+const semver = require('semver');
+const extend = require('deep-extend');
+const kebabCase = require('just-kebab-case');
 
 module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(`Welcome to the unreal ${chalk.red('generator-angular-electron')} generator!`)
-    );
+  initializing() {
+    this.props = {};
 
+    if (semver.lt(process.version, '8.9.0')) {
+      this.log(chalk.yellow('Angular CLI v6 needs NodeJS v8.9 or greater.'));
+      this.log(chalk.yellow(`You are using version ${ process.version  } which is not supported, please upgrade your version of NodeJS.\n`));
+      process.exit(-1);
+    }
+  }
+
+  prompting() {
     const prompts = [
       {
+        type: 'input',
+        name: 'name',
+        when: !this.props.name,
+        message: 'Project name:',
+        default: 'My Project',
+      },
+      {
+        type: 'input',
+        name: 'version',
+        when: !this.props.version,
+        message: 'Version:',
+        default: '0.1.0',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        when: !this.props.description,
+        message: 'Description:',
+      },
+      {
+        type: 'input',
+        name: 'githubAccount',
+        when: !this.props.githubAccount,
+        message: 'GitHub Username or Organization:',
+        default: this.user.github.username,
+      },
+      {
+        type: 'input',
+        name: 'authorName',
+        message: "Author's Name",
+        when: !this.props.authorName,
+        default: this.user.git.name(),
+        store: true,
+      },
+      {
+        type: 'input',
+        name: 'authorEmail',
+        message: "Author's Email",
+        when: !this.props.authorEmail,
+        default: this.user.git.email(),
+        store: true,
+      },
+      {
+        type: 'input',
+        name: 'authorUrl',
+        message: "Author's Homepage",
+        when: !this.props.authorUrl,
+        store: true,
+      },
+      {
+        type: 'input',
+        name: 'keywords',
+        message: 'Package keywords (comma to split)',
+        filter(words) {
+          return words.split(/\s*,\s*/g);
+        },
+      },
+      {
         type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
+        name: 'uikit',
+        message: 'Include UIkit?',
+        default: false, 
       }
     ];
 
     return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
       this.props = props;
     });
   }
 
+  default() {
+    this.destinationRoot(this.props.name);
+  }
+
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+    this.log(chalk.cyan('Copying files'));
+
+    const nonTemplatedFiles = [
+      { src: '_editorconfig', dest: '.editorconfig' },
+      { src: '_gitignore', dest: '.gitignore' },
+      { src: '_prettierrc', dest: '.prettierrc' },
+      { src: '_travis.yml', dest: '.travis.yml' },
+      { src: '_karma.conf.js', dest: 'karma.conf.js' },
+      { src: '_protractor.conf.js', dest: 'protractor.conf.js' },
+      { src: '_tsconfig.json', dest: 'tsconfig.json' },
+      { src: '_tslint.json', dest: 'tslint.json' },
+    ];
+
+    nonTemplatedFiles.forEach(file => {
+      this.fs.copy(this.templatePath(file.src), this.destinationPath(file.dest));
+    });
+
+    const templatedFiles = [
+      { src: '_README.md', dest: 'README.md' },
+      { src: '_LICENSE', dest: 'LICENSE' },
+      { src: '_package.json', dest: 'package.json' },
+      { src: '_angular.json', dest: 'angular.json' },
+      { src: 'e2e/**', dest: 'e2e' },
+      { src: 'src/**/*', dest: 'src' },
+    ];
+
+    templatedFiles.forEach(file => {
+      this.fs.copyTpl(
+        this.templatePath(file.src),
+        this.destinationPath(file.dest),
+        { props: this.props }
+      );
+    });
   }
 
   install() {
-    this.installDependencies();
+    this.log(`\nRunning ${chalk.yellow(`${this.packageManager} install`)}. This might take a couple minutes.`);
+    this.npmInstall(null, { loglevel: 'error' });
   }
+
+  end() {
+    this.log('\nInstallation complete. Get started with the following commands:')
+    this.log(`- $ ${chalk.green(`${this.packageManager} start`)}: start dev server with live reload inside your Eletron app.`);
+  }
+  
 };
---
+++ generators/app/templates/_README.md
@@ -0,0 +1,28 @@
+[![Travis Build Status][build-badge]][build]
+[![Dependencies Status][dependencyci-badge]][dependencyci]
+[![License][license-badge]](LICENSE.md)
+[![Watch on GitHub][github-watch-badge]][github-watch]
+[![Star on GitHub][github-star-badge]][github-star]
+[![Tweet][twitter-badge]][twitter]
+
+# <%= props.productName %>
+
+> <%= props.description %>
+
+More info to come
+
+---
+Created using the Yeoman [Angular Electron Generator](https://github.com/MarkRabey/generator-angular-electron)
+
+[build-badge]: https://img.shields.io/travis/<%= props.githubAccount %>/<%= props.name %>.svg?style=flat-square
+[build]: https://travis-ci.org/<%= props.githubAccount %>/<%= props.name %>?branch=master
+[dependencyci-badge]: https://dependencyci.com/github/<%= props.githubAccount %>/<%= props.name %>/badge
+[dependencyci]: https://dependencyci.com/github/<%= props.githubAccount %>/<%= props.name %>
+[license-badge]: https://img.shields.io/github/license/mashape/apistatus.svg?style=flat-square
+[license]: https://github.com/<%= props.githubAccount %>/<%= props.name %>/blob/master/LICENSE.md
+[github-watch-badge]: https://img.shields.io/github/watchers/<%= props.githubAccount %>/<%= props.name %>.svg?style=flat-square
+[github-watch]: https://github.com/<%= props.githubAccount %>/<%= props.name %>/watchers
+[github-star-badge]: https://img.shields.io/github/stars/<%= props.githubAccount %>/<%= props.name %>.svg?style=flat-square
+[github-star]: https://github.com/<%= props.githubAccount %>/<%= props.name %>/stargazers
+[twitter]: https://twitter.com/intent/tweet?text=Check%20out%20<%= props.name %>!%20https://github.com/<%= props.githubAccount %>/<%= props.name %>%20%F0%9F%91%8D
+[twitter-badge]: https://img.shields.io/twitter/url/https/github.com/<%= props.githubAccount %>/<%= props.name %>.svg?style=flat-square
