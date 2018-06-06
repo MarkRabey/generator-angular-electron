fa0aad82eb2825d983d4601109efc4d9486d9ba4
Mark Rabey <mark@markrabey.com>
2018-06-06 13:17:40 -0400

    version bump, added coveralls config

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,22 +1,7 @@
 'use strict';
-// Cconst path = require('path');
 const assert = require('yeoman-assert');
-// Cconst helpers = require('yeoman-test');
 
 describe('generator-angular-electron:app', () => {
-  // Comment
-  // beforeAll(() => {
-  //   return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({
-  //     productName: 'My Project',
-  //     version: '0.1.0',
-  //     githubAccount: 'MarkRabey',
-  //     authorName: 'Mark Rabey',
-  //     authorEmail: 'mark@markrabey.com',
-  //     authorUrl: 'https://markrabey.com',
-  //     uikit: true,
-  //   });
-  // });
-
   it('creates files', () => {
     assert.file(['package.json']);
   });
